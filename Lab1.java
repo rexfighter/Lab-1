518dacecf41c3d6e3125763166d55df36bada990
rexfighter <ayyogabe@gmail.com>
2016-08-08 22:00:38 -0700

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,56 @@
+package lab.pkg1;
+
+public class Lab1 {
+
+    public void age(int age) {
+        System.out.println("What is your age? " + age);
+    }
+    // age
+
+   /** Do not touch! I has question for you about this 
+    * 
+    public user(name) {
+       name = Butch
+    }   
+    
+    public getname(){
+        
+    } */
+    
+    public void annualIncome(int income) {
+        System.out.println("What is your annual income? " + income*2);
+    }
+    // income multiplied by two
+    
+    public void name(){
+        System.out.println("What is your name? Butch");
+               
+    }
+    // name name
+    
+    
+    public static void main(String[] args) {
+        Lab1 Butch= new Lab1();
+        
+        Butch.age(4);
+        Butch.annualIncome(4000);
+        Butch.name();
+        
+        int age;
+        age = 4;
+        int annualIncome;
+        annualIncome = 4000;
+        
+        System.out.println("Hello Butch. Your age is " + age 
+                + " and your income is " + annualIncome*2 + ".");
+        
+        
+        
+        
+                
+                
+        
+        
+        
+        //You want me to write that whole sentence?? you crazy
+}
